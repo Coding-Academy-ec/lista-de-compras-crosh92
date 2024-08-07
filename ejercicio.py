b6455b1f75ad8f92bfdfad07a2273cd79dd46983
crosh92 <jonathan.visconti@logex.ec>
2024-08-06 20:12:00 -0500

    asignacion completada2

--- ejercicio.py
+++ ejercicio.py
@@ -1,11 +1,12 @@
 # Ingrese una lista de compras: manzanas, plátanos, leche, pan
 # Los productos en la lista de compras son: ['manzanas', 'plátanos', 'leche', 'pan']
 
-compras = input("Ingrese una lista de compras: ")
-productos = compras.split(", ")
+# compras = input("Ingrese una lista de compras: ")
+# productos = compras.split(", ")
+productos=['manzanas', 'plátanos', 'leche', 'pan']
 print(f"Los productos en la lista de compras son: {productos}")
 
 # Convertir la lista de compras en una tupla
 def convertir_lista_a_tupla(lista):
-    mi_tupla = tuple(lista)
-    return mi_tupla
+    tupla_productos = tuple(lista)
+    return tupla_productos
