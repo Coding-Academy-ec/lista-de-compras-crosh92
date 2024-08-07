786a44a51716b1e767ab5b7fce173bbd8c8bc16c
crosh92 <jonathan.visconti@logex.ec>
2024-08-06 20:06:55 -0500

    asignacion completada

--- ejercicio.py
+++ ejercicio.py
@@ -7,4 +7,5 @@ print(f"Los productos en la lista de compras son: {productos}")
 
 # Convertir la lista de compras en una tupla
 def convertir_lista_a_tupla(lista):
-    return # Utiliza una función para convertir 
+    mi_tupla = tuple(lista)
+    return mi_tupla
